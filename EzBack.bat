db74178c4f89878dc3f2be7f0f896125a9369796
Qnezor <qnezoru@gmail.com>
2022-11-23 20:38:27 +0300

    Update EzBack.bat

--- EzBack.bat
+++ EzBack.bat
@@ -3,16 +3,18 @@ color F0
 cls
 
 @echo off
-set er=Error #1. Incorrect choice of system or nosystem.
-set pr=Please restart the program
+set er= Error #1. Incorrect choice.
+set pr= Please restart the program
+set ezbi= EzBack - a free add-on for adb
+set link= https://github.com/Qnezor/EzBack
+set exit= Press ENTER to exit
 
-echo    ____    ___           __     ____  ___ 
-echo   / __/__ / _ )___ _____/ /__  / / / / _ \
-echo  / _//_ // _  / _ `/ __/  '_/ /_  _// // /
-echo /___//__/____/\_,_/\__/_/\_\   /_/(_)___/                                                                                                                                                                                                          
+echo     ____    ___           __     ____   __
+echo    / __/__ / _ )___ _____/ /__  / / /  / /
+echo   / _//_ // _  / _ `/ __/  '_/ /_  _/ / / 
+echo  /___//__/____/\_,_/\__/_/\_\   /_/(_)_/  
 echo.
-echo.
-echo Press ENTER to start
+echo  Press ENTER to start
 set /p enter=
 
 echo.
@@ -21,18 +23,20 @@ cls
 set adbfolder=C:\platform-tools
 goto a1
 ) else (
-echo adb.exe not found
-echo Specify the path to the platform-tools folder
-echo Example: C:\platform-tools
+echo.
+echo  adb.exe not found
+echo  Specify the path to the platform-tools folder
+echo  Example: C:\platform-tools
 set /p adbfolder="-"
 )
 
 :a1
 cls
-echo The backup will be saved to C:\EzBackups
 echo.
-echo y - Do you agree
-echo n - Change path
+echo  The backup will be saved to C:\EzBackups
+echo.
+echo  y - Do you agree
+echo  n - Change path
 set /p folderchoice="-"
 
 if %folderchoice% equ y (
@@ -48,8 +52,8 @@ goto a2
 )
 if %folderchoice% equ n (
 echo.
-echo Specify a folder for saving
-echo Example: C:\Users\User\Desktop
+echo  Specify a folder for saving
+echo  Example: C:\Users\User\Desktop
 set /p SaveFolder="-"
 goto a2
 )
@@ -57,56 +61,74 @@ goto a2
 if not %folderchoice% equ y (
 echo.
 echo %er%
-echo.
 echo %pr%
 echo.
-echo EzBack - a free add-on for platform-tools
 echo.
-echo https://github.com/Qnezor/EzBack
+echo %ezbi%
 echo.
-pause
+echo %link%
+echo.
+echo %exit%
+set /p exit1=
+goto exit2
 )
 
 :a2
 cls
-echo Give the file a name
+echo.
+echo  Give the file a name
 set /p FlieName="-"
 
 cls
-echo y - system
-echo n - nosystem
+echo.
+echo  y - system
+echo  n - nosystem
 set /p D="-"
 
 if %D% equ y (
 cls
 start cmd.exe color F0 /k %adbfolder%\adb backup -apk -shared -all -system -f %SaveFolder%\system-%FlieName%.ba
-echo Backup is started with the condition system
 echo.
-echo EzBack - a free add-on for platform-tools
+echo  Backup is started with the condition system
 echo.
-echo https://github.com/Qnezor/EzBack
 echo.
-pause
+echo %ezbi%
+echo.
+echo %link%
+echo.
+echo %exit%
+set /p exit1=
+goto exit2
 )
 if %D% equ n (
 cls
 start cmd.exe color F0 /k %adbfolder%\adb backup -apk -shared -all -nosystem -f %SaveFolder%\nosystem-%FlieName%.ba
-echo Backup is started with the condition nosystem
 echo.
-echo EzBack - a free add-on for platform-tools
+echo  Backup is started with the condition nosystem
 echo.
-echo https://github.com/Qnezor/EzBack
 echo.
-pause
+echo %ezbi%
+echo.
+echo %link%
+echo.
+echo %exit%
+set /p exit1=
+goto exit2
 )
-if not %D% equ n or not %D% equ y (
-cls
+if not %D% equ y (
+echo.
 echo %er%
 echo %pr%
 echo.
-echo EzBack - a free add-on for platform-tools
 echo.
-echo https://github.com/Qnezor/EzBack
+echo %ezbi%
 echo.
-pause
+echo %link%
+echo.
+echo %exit%
+set /p exit1=
+goto exit2
 )
+
+:exit2
+exit
